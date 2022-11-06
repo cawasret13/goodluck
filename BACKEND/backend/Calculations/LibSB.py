9f478d9cc9aece0ed5c792954d54dd54dfff13df
cawasret <cawasret13@mail.ru>
2022-11-06 22:23:20 +0300

    Формирование файла

--- BACKEND/backend/Calculations/LibSB.py
+++ BACKEND/backend/Calculations/LibSB.py
@@ -1,7 +1,10 @@
+import ast
 import random
-
 import openpyxl
+from openpyxl.utils import get_column_letter
 from soupsieve.util import lower
+from Calculations.models import FileData
+
 
 def transliterate(text):
     result = ''
@@ -27,6 +30,39 @@ def generateID():
     return id
 
 def createFile(id_session):
+    dataFIle = FileData.objects.get(id_session = id_session)
+
     book = openpyxl.Workbook()
     sheet = book.active
-    sheet["A1"] = "Локация"
+    sheet["A1"] = "Местоположение"
+    sheet["B1"] = "Количество комнат"
+    sheet["C1"] = "Сегмент"
+    sheet["D1"] = "Этажность дома"
+    sheet["E1"] = "Материал стен"
+    sheet["F1"] = "Этаж расположения"
+    sheet["G1"] = "Площадь квартиры, кв.м"
+    sheet["H1"] = "Площадь кухни, кв.м"
+    sheet["I1"] = "Наличие балкона/лоджии"
+    sheet["J1"] = "Удаленность от станции метро, мин. пешком"
+    sheet["K1"] = "Состояние"
+    sheet["L1"] = "Цена"
+    r = 2
+    sheet.
+    for cell in ast.literal_eval(dataFIle.NewData):
+        sheet[r][0].value =cell["location"]
+        sheet[r][1].value =cell["numRooms"]
+        sheet[r][2].value =cell["segment"]
+        sheet[r][3].value =cell["floorsHouse"]
+        sheet[r][4].value =cell["materialWall"]
+        sheet[r][5].value =cell["floor"]
+        sheet[r][6].value =cell["areaApart"]
+        sheet[r][7].value =cell["areaKitchen"]
+        sheet[r][8].value =cell["balcony"]
+        sheet[r][9].value =cell["proxMetro"]
+        sheet[r][10].value =cell["structure"]
+        sheet[r][11].value =cell["price"]
+        r+=1
+
+    file = book.save(f"static/Export_{id_session.replace('|', '')}.xlsx")
+    book.close()
+
